1724f04a314372c3d6ff923bde239668b98a19b2
nikolovaradica <nikolova.radica@gmail.com>
2024-06-26 13:22:13 +0200

    added application files

---
+++ src/main/java/mk/ukim/finki/wp/exam/example/model/Category.java
@@ -0,0 +1,37 @@
+package mk.ukim.finki.wp.exam.example.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
